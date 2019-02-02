2cad602128f64724a307b844eef9af2e104df61f
Andrew Briand <atb8888@comcast.net>
2019-02-01 21:15:42 -0800

    Physics

--- Physics.lua
+++ Physics.lua
@@ -1,3 +1,5 @@
+require 'Utils'
+
 function Physics()
 	local physics = {}
 	physics.update = physicsUpdate
@@ -5,11 +7,20 @@ function Physics()
 end
 
 function physicsUpdate(physics, dt)
+	print("physicsUpdate")
 	for key, bullet in pairs(objects.bullets) do
 		for key2, pigeon in pairs(objects.pigeons) do
 			--Bullet and pigeon are colliding
+			print(dist(bullet.x, bullet.y, pigeon.x, pigeon.y))
 			if  dist(bullet.x, bullet.y, pigeon.x, pigeon.y) < (bullet.radius + pigeon.radius) then
-				
+				bulletToPigeon = {}
+				bulletToPigeon.x = 2*(pigeon.x - bullet.x)
+				bulletToPigeon.y = 2*(pigeon.y - bullet.y)
+				pigeon.velocity.x = pigeon.velocity.x + bulletToPigeon.x
+				pigeon.velocity.y = pigeon.velocity.y + bulletToPigeon.y
+				bullet.velocity.x = bullet.velocity.x - bulletToPigeon.x
+				bullet.velocity.y = bullet.velocity.y - bulletToPigeon.y
+				print("Collision")
 			end
 		end
 	end
