b305a8619b96f361b1c7bf5e5257aad4c4bacd93
Andrew Briand <atb8888@comcast.net>
2019-02-01 20:05:45 -0800

    Update Pigeon.lua

--- Pigeon.lua
+++ Pigeon.lua
@@ -1,14 +1,18 @@
-function Pigeon(x, y)
+function Pigeon(x, y, velocity)
 	local pigeon = {}
 	pigeon.update = pigeonUpdate
 	pigeon.draw = pigeonDraw
 	pigeon.x = x
 	pigeon.y = y
+	pigeon.velocity = {}
+	pigeon.velocity.x = 0
+	pigeon.velocity.y = 0
 	return pigeon
 end
 
 function pigeonUpdate(pigeon)
-	
+	pigeon.x = pigeon.x + velocity.x
+	pigeon.y = pigeon.y + velocity.y
 end
 
 function pigeonDraw(pigeon)
