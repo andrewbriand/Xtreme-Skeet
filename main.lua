f41d86be8af5d59bef90db589aec44693fd2cc2e
Andrew Briand <atb8888@comcast.net>
2019-02-01 19:39:38 -0800

    Created objects table

--- main.lua
+++ main.lua
@@ -1,5 +1,12 @@
-function love.load()
 
+function love.load()
+	objects = {}
+	objects.pigeons = {}
+	objects.players = {}
+	objects.bullets = {}
+	objects.fragments = {}
+	objects.physics = Physics()
+	objects.pigeonLauncher = PigeonLauncher()
 end
 
 function love.update(dt)
