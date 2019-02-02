e505f8e31ba076d13a8bcabb466859339410a17c
David Krug <davidkrug00@gmail.com>
2019-02-01 21:07:23 -0800

    Update player.lua

--- player.lua
+++ player.lua
@@ -1,17 +1,29 @@
 PLAYER_SPEED = 1000
+PLAYER_ROTATION_SPEED = 5
 PLAYER_RADIUS = 10
 PLAYER_FRICTION = .9
+
+math.randomseed(os.time())
+math.random()
+math.random()
+math.random()
 
 PLAYER_CONTROLS = {{
 					up = "w",
 					down = "s",
 					left = "a",
 					right = "d",
+					clockwise = "e",
+					counterClockwise = "q",
+					shoot = "shift",
 				  },{
-					up = "up",
-					down = "down",
-					left = "left",
-					right = "right",
+					up = "i",
+					down = "k",
+					left = "j",
+					right = "l",
+					clockwise = "u",
+					counterClockwise = "o",
+					shoot = ";"
 				  }}
 
 function Player(name, controlSet)
@@ -27,10 +39,12 @@ function Player(name, controlSet)
 		name = name,
 		radius = PLAYER_RADIUS,
 		speed = PLAYER_SPEED,
+		rotationSpeed = PLAYER_ROTATION_SPEED,
 		controls = controls,
 		color = color,
+		ammo = 0,
 		
-		x = 100,
+		x = math.random()*200,
 		y = 150,
 		dx = 100,
 		dy = 150,
@@ -46,24 +60,39 @@ function updatePlayer(self, dt)
 	self.x = self.x + self.dx * dt
 	self.y = self.y + self.dy * dt
 	
-	if (love.keyboard.isDown(controls.down)) then
+	-- lateral
+	if (love.keyboard.isDown(self.controls.down)) then
 		self.dy = self.dy + self.speed * dt
 	end
-	if (love.keyboard.isDown(controls.up)) then
+	if (love.keyboard.isDown(self.controls.up)) then
 		self.dy = self.dy - self.speed * dt
 	end
-	if (love.keyboard.isDown(controls.right)) then
+	if (love.keyboard.isDown(self.controls.right)) then
 		self.dx = self.dx + self.speed * dt
 	end
-	if (love.keyboard.isDown(controls.left)) then
+	if (love.keyboard.isDown(self.controls.left)) then
 		self.dx = self.dx - self.speed * dt
+	end
+	
+	-- rotation
+	if (love.keyboard.isDown(self.controls.clockwise)) then
+		self.dir = self.dir + self.rotationSpeed * dt
+	end
+	if (love.keyboard.isDown(self.controls.counterClockwise)) then
+		self.dir = self.dir - self.rotationSpeed * dt
 	end
 	
 	self.dx = self.dx * PLAYER_FRICTION
 	self.dy = self.dy * PLAYER_FRICTION
+	
+end
+
+function shootPlayer(self)
+	
 end
 
 function drawPlayer(self)
 	love.graphics.setColor(self.color)
+	love.graphics.line(self.x, self.y, self.x + math.cos(self.dir) * self.radius * 2, self.y + math.sin(self.dir) * self.radius * 2)
 	love.graphics.circle("fill", self.x, self.y, self.radius)
 end
